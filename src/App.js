b2054165dbcea0a8ff69995759faf6cbb9232b4a
Hà Ngọc Kiên <62798483+kienyataome99@users.noreply.github.com>
2021-04-04 18:06:53 +0700

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default App
-=======
 import React from 'react';
 import Slider from './pages/HomePage/Slider';
 import HomeContent from './pages/HomePage/HomeContent';
 import HeaderPage from './pages/Header/HeaderPage';
 import { Layout, Menu, Breadcrumb, Table } from 'antd';
-
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
-
 const App = () => {
 	return (
 		<div>
@@ -39,4 +24,4 @@ const App = () => {
 };
 
 export default App;
->>>>>>> dcd9afe4f4c641c8fbf37b79c46fa2fb1b20980c
+
